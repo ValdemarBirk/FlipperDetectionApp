10431d8da0df4754b5c5708828525c0988117616
AlexanderKoefoed <alexanderkoefoed@gmail.com>
2023-11-16 11:56:09 +0100

    initial try for wigle query

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.API.WigleAPIService
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +15,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         startActivity(Intent(this@MainActivity,BluetoothLEController::class.java ));
+        runBlocking {
+            launch {
+            val retrofitBuilder = Retrofit.Builder().baseUrl("https://api.wigle.net/api")
+                .addConverterFactory(GsonConverterFactory.create()).build()
+            val service = retrofitBuilder.create(WigleAPIService::class.java)
+            val currentMac = "";
+            val currentFlipper = service.getBLEDeviceHistory(currentMac, false, true, true)
+            }
+        }
+        /*
+                val bluetoothLEController : BluetoothLEController = BluetoothLEController();
+                bluetoothLEController.scanLeDevice();
 
-/*
-        val bluetoothLEController : BluetoothLEController = BluetoothLEController();
-        bluetoothLEController.scanLeDevice();
 
-
- */
+         */
     }
 
 
